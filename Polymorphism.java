93d44ed4f8efa5a559fb1b7d0b873333fa720c8d
tayyab_20 <tayyabali78013@gmail.com>
2024-10-24 00:21:59 +0500

    first commit

---
+++ Polymorphism.java
@@ -0,0 +1,25 @@
+// class Student {
+//     String name;
+//     int age;
+
+//     public void bioData(String name) {
+//         System.out.println("My name is " + name);
+//     }
+
+//     public void bioData(int age) {
+//         System.out.println("My age is " + age);
+//     }
+
+//     public void bioData(String name, int age) {
+//         System.out.println("My name is " + name + " & My age is " + age);
+//     }
+// }
+
+// public class Polymorphism {
+//     public static void main(String args[]) {
+//         Student s1 = new Student();
+//         s1.name = "Tayyab Ali";
+//         s1.age = 21;
+//         s1.bioData(s1.name, s1.age);
+//     }
+// }
---
+++ Student.java
@@ -0,0 +1,24 @@
+// class StudentInfo {
+//     String name;
+//     int rollno;
+//     String field;
+//     String subject;
+
+//     public void bioData() {
+//         System.out.println("Name: " + name);
+//         System.out.println("Roll Number: " + rollno);
+//         System.out.println("Field: " + field);
+//         System.out.println("Subject: " + subject);
+//     }
+// }
+
+// public class Student {
+//     public static void main(String args[]) {
+//         StudentInfo student1 = new StudentInfo();
+//         student1.name = "Tayyab Ali";
+//         student1.rollno = 003;
+//         student1.field = "Software Engineering";
+//         student1.subject = "Java Programming";
+//         student1.bioData();
+//     }
+// }
